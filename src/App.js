f794252d2365fba92d194255509f5acf14a1b143
Flora <floraisabell@gmail.com>
2019-11-29 17:35:50 -0800

    fixing structure errors

--- src/App.js
+++ src/App.js
@@ -2,69 +2,50 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './App.css';
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
+class App extends Component {
+  constructor() {
+    super();
     this.state = {
-      error: null,
-      isLoaded: false,
       items: []
     };
   }
 
   componentDidMount() {
     fetch("https://itunes.apple.com/search?term=jack+johnson&limit=25")
-      .then(res => res.json())
-      .then(
-        (result) => {
-          
-          console.log(result);
-          
-          this.setState({
-            isLoaded: true,
-            items: result
-          });
-        },
-        // Note: it's important to handle errors here
-        // instead of a catch() block so that we don't swallow
-        // exceptions from actual bugs in components.
-        (error) => {
-          this.setState({
-            isLoaded: true,
-            error
-          });
-        }
-      )
+      .then(results) => {
+          return results.json();
+        }).then(data => {
+          let items = data.results.map((item) => {
+            return(
+              <div key={item.results}>
+                <img src={item.results.} />
+              </div>
+            )
+          })
+        (items) => {
+          this.setState({items : items});
+          console.log("state", this.state.items);
+        })
   }
 
   render() {
-    const { error, isLoaded, items } = this.state;
-
-    if (error) {
-      return <div>Error: {error.message}</div>;
-    } else if (!isLoaded) {
-      return <div>Loading...</div>;
-    } else {
       return (
-        
-        <div>
-        
-          {items.map(item => (
-
-          <p>{item.artistName} ({item.trackName})</p> 
-          ))}
+        <div className= "bodydiv">
+        <div className= "titlediv">
+          <h1 classname= "AN"> Artists Name </h1>
           
-      </div>  
-        
+        </div>
+        <div className= "container1">
+          <div> className= "container2">
+            {this.state.items}
+          </div>
+          <div> </div>
+        </div>  
+        </div>
       );
     }
   }
 }
 
-
-ReactDOM.render(
-  <App />,
-  document.getElementById("app")
-);
 
 export default App;
